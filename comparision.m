78be56226c5c96078341c06b9fc70c4e06bee160
unknown <1062859574@qq,com>
2020-07-01 17:38:27 +0800

    change distance from number to bar for better visualization

--- comparision.m
+++ comparision.m
@@ -11,14 +11,14 @@ num = [l95 l09 l16];
 
 new_arr = [norm_1995(1:val);norm_2009(1:val);norm_2016(1:val)]';
 
-%%
+%% plot
 close all
 figure('Name','algorithm comparison','position',[500 100 1280 800])
 bar(new_arr)
 for i = 1:val
-    text(i-0.35,new_arr(i,1)+5,num2str(new_arr(i,1),'%3.2f'),'fontsize',10);
-    text(i-0.09,new_arr(i,2)+5,num2str(new_arr(i,2),'%3.2f'),'fontsize',10);
-    text(i+0.1,new_arr(i,3)+5,num2str(new_arr(i,3),'%3.2f'),'fontsize',10);
+    text(i-0.325,new_arr(i,1)+max(max(new_arr))/40,num2str(new_arr(i,1),'%6.2f'),'fontsize',10);
+    text(i-0.09,new_arr(i,2)+max(max(new_arr))/40,num2str(new_arr(i,2),'%6.2f'),'fontsize',10);
+    text(i+0.1,new_arr(i,3)+max(max(new_arr))/40,num2str(new_arr(i,3),'%6.2f'),'fontsize',10);
 end
 legend('POE result','MPOE result','ACS result')
 title('Comparison of three algorithms')
